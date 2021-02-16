a43ebc90812c439fe9dd8a8e7906ea99ccc46188
Peggy Karani <karanipeggy65@gmail.com>
2021-02-16 15:30:13 +0000

    Add function that swaps numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,6 +4,8 @@
  * swap_int - swaps the value of two integers
  * @a: pointer a
  * @b: pointer b
+ *
+ *Return: void
  */
 void swap_int(int *a, int *b)
 {
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,1 +1,2 @@
+void swap_int(int *a, int *b);
 void reset_to_98(int *n);
