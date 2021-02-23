2a8d4a73e9893012ae8d32598b9fcbf9d5f6ca24
Peggy Karani <karanipeggy65@gmail.com>
2021-02-23 08:21:24 +0000

    Function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * print_chessboard - function
+  * @a : variable
+  */
+
+void print_chessboard(char (*a)[8])
+{
+int i, c;
+for (i = 0; i < 8; i++)
+{
+for (c = 0; c < 8; c++)
+{
+putchar(a[i][c]);
+}
+putchar('\n');
+}
+}
+
+
